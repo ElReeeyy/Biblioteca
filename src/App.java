b904a820caa0af6b757a329f346d666af6e64cfb
ElReeeyy <areyt01@iesalbarregas.es>
2025-01-23 17:48:21 +0100

    Creacion clase libro

--- src/App.java
+++ src/App.java
@@ -1,4 +1,9 @@
 public class App {
+    /**
+     * @author Alejandro Rey Tostado
+     * @author Alberto Garcia Izquierdo
+     */
+
     public static void main(String[] args) throws Exception {
         System.out.println("Creacion");
     }
---
+++ src/Libros.java
@@ -0,0 +1,24 @@
+public class Libros {
+    private String nombre;
+    private String autor;
+    private String categoria;
+
+
+    public Libros(String nombre, String autor, String categoria) {
+        this.nombre = nombre;
+        this.autor = autor;
+        this.categoria = categoria;
+    }
+    
+
+    public void agregarLibros() {}
+
+    public void eliminarLibro() {}
+
+    public void buscarLibroTitulo() {}
+
+    public void buscarLibroAutor() {}
+
+    public void buscarLibroCategoria() {}
+
+}
