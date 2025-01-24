8288d316b4883896fd653ae350888dfd1e05374d
agarciai03 <agarciai03@iesalbarregas.es>
2025-01-24 12:30:04 +0100

    Clase Usuario

--- src/Usuarios.java
+++ src/Usuarios.java
@@ -1,3 +1,13 @@
 public class Usuarios {
+    private String nombre;
+    private String contrasenia;
+    private boolean esAdmin;
+
+    public Usuarios(String nombre, String contrasenia, boolean esAdmin) {
+        this.nombre = nombre;
+        this.contrasenia = contrasenia;
+        this.esAdmin = esAdmin;
+    }
+
     
 }
