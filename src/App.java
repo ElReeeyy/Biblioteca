435d3247e5571a2dcc63c091fb29c5a1c0e6160f
ElReeeyy <areyt01@iesalbarregas.es>
2025-01-24 12:30:45 +0100

    Menu

--- src/App.java
+++ src/App.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
+
 public class App {
     /**
      * @author Alejandro Rey Tostado
      * @author Alberto Garcia Izquierdo
      */
 
+     public static final Scanner teclado = new Scanner(System.in);
+
+     public static String menu() {
+        System.out.println("--- BIENVENIDO A SU BIBLIOTECA FAVORITA ---");
+        System.out.println("1. INICIAR SESION");
+        System.out.println("2. SALIR");
+    
+        return teclado.nextLine();
+    }
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Creacion");
+        boolean salir = false;
     }
 }
