dde22cde735400b10751232fbe9645513334cdad
ElReeeyy <areyt01@iesalbarregas.es>
2025-01-28 20:11:49 +0100

    Creacion objetos

--- src/App.java
+++ src/App.java
@@ -19,8 +19,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         boolean salir = false;
 
+        // CREACION DE USUARIOS
         Usuarios usuario = new Usuarios("Alejandro", "admin", true);
         Usuarios usuario2 = new Usuarios("Alberto", "1234", false);
+        Usuarios usuario3 = new Usuarios("Pepe", "1234", false);
+        Usuarios usuario4 = new Usuarios("Miguel", "1234", false);
+        Usuarios usuario5 = new Usuarios("Jose Antonio", "1234", false);
+
+        // CREACION DE LIBROS
+        Libros libro1 = new Libros("El señor de los anillos", "J.R.R. Tolkien", Categoria.NOVELA, false);
+        Libros libro2 = new Libros("It", "Stephen King", Categoria.TERROR, false);
+        Libros libro3 = new Libros("La guerra de los mundos", "H.G. Wells", Categoria.CIENCIA_FICCION, true);
+        Libros libro4 = new Libros("La historia interminable", "Michael Ende", Categoria.INFANTIL, true);
+        Libros libro5 = new Libros("El principito", "Antoine de Saint-Exupéry", Categoria.JUVENIL, false);
 
         do {
             switch (menu()) {
